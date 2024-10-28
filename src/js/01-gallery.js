542a1f06f72f0b4e7021179ec92645af28d63cb1
metalstyle545@gmail.com <131258361+AlexLis312@users.noreply.github.com>
2024-10-28 22:05:21 +0200

    q

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -28,39 +28,11 @@ function createGalleryImages(items) {
     .join('');
 }
 
-galleryList.addEventListener('click', onImageClick);
-
-function onImageClick(event) {
+galleryList.addEventListener('click', event => {
   event.preventDefault();
-  const galleryImage = event.target;
-  if (galleryImage.nodeName === 'IMG') {
-    creareModalFromImage(galleryImage.dataset.source);
-  }
-}
+});
 
-function creareModalFromImage(image) {
-  const instance = SimpleLightbox.create(
-    `
-    <div class="modal">
-        <img src="${image}">
-    </div>
-`,
-    {
-      onShow: instance => {
-        document.addEventListener('keydown', onEscKeyPress);
-      },
-      onClose: instance => {
-        document.removeEventListener('keydown', onEscKeyPress);
-      },
-    }
-  );
-
-  instance.show();
-
-  addEventListener('keydown', onEscKeyPress);
-  function onEscKeyPress(event) {
-    if (event.code === 'Escape') {
-      instance.close();
-    }
-  }
-}
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
