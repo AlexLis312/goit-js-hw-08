a61bb7d32e7fc4c7d6a6e1adbc5050658883ce73
metalstyle545@gmail.com <131258361+AlexLis312@users.noreply.github.com>
2024-10-30 18:50:53 +0200

    qq

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const videoIframe = document.querySelector('#vimeo-player');
+const player = new Vimeo(videoIframe);
+
+const savedPlaybackTime = localStorage.getItem('videoplayer-current-time');
+
+const updatePlaybackTime = function (event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+
+player.on('timeupdate', throttle(updatePlaybackTime, 1000));
+
+if (savedPlaybackTime) {
+  player.setCurrentTime(savedPlaybackTime);
+}
+s;
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+let feedbackData = {};
+
+const form = document.querySelector('form');
+const input = document.querySelector('form input');
+const textarea = document.querySelector('form textarea');
+
+form.addEventListener('input', throttle(localStorageFeedback, 500));
+form.addEventListener('submit', onSubmit);
+
+const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+localStoragePopulateForm(savedData);
+
+function localStoragePopulateForm(data) {
+  if (data !== null) {
+    input.value = data.email || '';
+    textarea.value = data.message || '';
+    feedbackData = data;
+  }
+}
+
+function localStorageFeedback() {
+  feedbackData = {
+    email: input.value,
+    message: textarea.value,
+  };
+
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(feedbackData));
+}
+
+function onSubmit(event) {
+  event.preventDefault();
+  console.log(feedbackData);
+  localStorage.removeItem(STORAGE_KEY);
+  form.reset();
+}
